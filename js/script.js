0b6bb0950f3bab3d5b4e78c2efa72e0735b36b6c
Oleg <olegsameluk82@gmail.com>
2021-12-06 20:21:46 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+
+
+// let number = 5;
+
+
+// console.log(5/0);
+// const leftBorderWidth = 1;
+
+// number = 10;
+
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+// console.log(obj.a);
+
+// const obj = {
+//     name: "Oleg",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(obj.name)
+
+// alert("hell");
+
+// const resalt = confirm("You");
+// console.log(resalt)
+
+
+const answer = prompt("Вам 18?", "18")
+console.log(answer)
